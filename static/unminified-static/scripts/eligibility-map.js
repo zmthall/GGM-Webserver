01aa8c5404d27d9d70796ec2371ffa8ce1fa704c
zmthall <zmthall@gmail.com>
2024-07-05 15:28:39 -0600

    Added an expanded map to the nemt-page might change later as it is over complicated but works

---
+++ static/unminified-static/scripts/eligibility-map.js
@@ -0,0 +1,45 @@
+class MapHandler {
+    constructor() {
+        this.map = {
+            section: document.querySelector('[data-eligible-places-container]'),
+            modalOpen: document.querySelector('[data-map-container]'),
+            wrapper: document.querySelector('[data-map-container]').cloneNode(true),
+            container: document.createElement('div'),
+            button: document.createElement('button'),
+        }
+
+        this.map.modalOpen.addEventListener('click', () => this.modalInjection())
+    }
+
+    containerSetup() {
+        this.map.container.classList.add("eligibility-map", "pop-up-modal")
+        this.map.container.setAttribute('data-map-modal-container', '')
+        this.map.button.classList.add('close-button')
+        this.map.button.setAttribute('data-close-button', '')
+        this.map.button.innerHTML = 'X'
+
+        this.map.wrapper.setAttribute("class", "eligibility-map-wrapper pop-up-modal")
+        
+        this.map.container.append(this.map.button)
+        this.map.container.append(this.map.wrapper)
+
+        this.map.button.addEventListener('click', () => this.modalDelete())
+
+        return this.map.container
+    }
+
+    modalInjection() {
+        this.map.section.append(this.containerSetup())
+        document.body.classList.add('no-scroll')
+        this.map.container = document.querySelector('[data-map-modal-container]')
+    }
+
+    modalDelete() {
+        this.map.container.remove()
+        document.body.classList.remove('no-scroll')
+        this.map.container = document.createElement('div')
+    }
+}
+
+const mapHandler = new MapHandler()
+console.log(mapHandler)

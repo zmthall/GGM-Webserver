35d7471707c3ffe6b82c2bba07b344f7804ffb1c
zmthall <zmthall@gmail.com>
2024-07-08 15:58:52 -0600

    added webcrawl to the webserver

---
+++ webcrawl.js
@@ -0,0 +1,47 @@
+const cheerio = require('cheerio')
+const axios = require("axios"); 
+
+// function containsValues(str, object) {
+//     console.log(str.includes('\n') || /\d/.test(str) || object[str].includes('https://'))
+// }
+
+function objTest(key, newValue, object) {
+    var keyTest = key.includes('\n') || /\d/.test(key) || key.includes('@')
+    var valueTest = newValue.includes('https') || Object.values(object).includes(newValue)
+
+    if(key === 'policy')
+        console.log(object[key])
+    
+    return valueTest || keyTest
+}
+
+async function WebCrawl() {
+    console.log('\n\n\n\n\n-------------------------------------------------------------------')
+    var pageHTML = await axios.get("localhost")
+    var $ = cheerio.load(pageHTML.data)
+    const pageURLS = {}
+
+    $("a").each((index, element) => {
+        var elInnerText = $(element).text().toLowerCase().replaceAll('-', '_').replaceAll(' ', '_')
+        if(!objTest(elInnerText, $(element).attr('href'), pageURLS)) {
+            pageURLS[elInnerText] = $(element).attr('href')
+        }
+    })
+
+    console.log(pageURLS)
+    
+    for(const idx in pageURLS) {
+        pageHTML = await axios.get(`${pageURLS[idx]}`)
+        $ = cheerio.load(pageHTML.data)
+        $("a").each((index, element) => {
+            var elInnerText = $(element).text().toLowerCase().replaceAll('-', '_').replaceAll(' ', '_')
+            if(!objTest(elInnerText, $(element).attr('href'), pageURLS)) {
+                pageURLS[elInnerText] = $(element).attr('href')
+            }
+        })
+    }
+
+    console.log(pageURLS)
+}
+
+WebCrawl()

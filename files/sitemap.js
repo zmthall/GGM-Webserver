97ca507af5c93802d47c3f6ce82f739be888bff1
zmthall <zmthall@gmail.com>
2024-07-08 21:06:15 -0600

    Added Sitemap package to add a sitemap.xml file

--- files/sitemap.js
+++ files/sitemap.js
@@ -1,49 +1,32 @@
 const cheerio = require('cheerio')
-const axios = require("axios"); 
+const axios = require("axios")
 
-// function containsValues(str, object) {
-//     console.log(str.includes('\n') || /\d/.test(str) || object[str].includes('https://'))
-// }
-
-function objTest(key, newValue, object) {
-    var keyTest = key.includes('\n') || /\d/.test(key)
-    var valueTest = newValue.includes('https') || Object.values(object).includes(newValue)
-
-    if(key === 'policy')
-        console.log(object[key])
-    
-    return valueTest || keyTest
-}
-
-async function WebCrawl() {
+async function webCrawl() {
     console.log('\n\n\n\n\n-------------------------------------------------------------------')
     var pageHTML = await axios.get("localhost")
     var $ = cheerio.load(pageHTML.data)
     const pageURLS = {}
 
-    $("a:not([ref=nofollow])").each((index, element) => {
+    $("a:not([rel~='nofollow'])").each((index, element) => {
         var elInnerText = $(element).text().toLowerCase().replaceAll('-', '_').replaceAll(' ', '_')
         if(Object.values(pageURLS).length === 0 || Object.values(pageURLS).length != 0 && !Object.values(pageURLS).includes($(element).attr('href'))) {
             pageURLS[elInnerText] = $(element).attr('href')
         }
     })
-
-    // console.log(pageURLS)
     
     var page = []
     for(const idx in pageURLS) {
         pageHTML = await axios.get(`${pageURLS[idx]}`)   
         $ = cheerio.load(pageHTML.data)
-        $("a:not([ref=nofollow])").each((index, element) => {
+        $("a:not([rel~='nofollow'])").each((index, element) => {
             var elInnerText = $(element).text().toLowerCase().replaceAll('-', '_').replaceAll(' ', '_')
             if(!Object.values(pageURLS).includes($(element).attr('href'))) {
-                pageURLS[elInnerText] = $(element).attr('href') + `[${pageURLS[idx]}]`
+                pageURLS[elInnerText] = $(element).attr('href')
             }
         })
     }
-
-    console.log(pageURLS)
-    console.log(page)
 }
 
-WebCrawl()
+module.exports = {
+    
+}

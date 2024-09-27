8fe968a00a556093825eec66d374a41187410b42
zmthall <zmthall@gmail.com>
2024-09-27 14:21:04 -0600

    Added recaptcha functionality to the application form

--- files/google-handler.js
+++ files/google-handler.js
@@ -34,57 +34,57 @@ class GoogleAPIHandler {
         });
     }
 
-async getSheetsRows(sheet = 'Sheet1') {
-    const googleSheets = google.sheets({version: 'v4', auth: await this.getSheetsAuth()});
-    return (await googleSheets.spreadsheets.values.get({
-        auth: this.auth,
-        spreadsheetId: this.spreedsheetId,
-        range: sheet
-    })).data;
-}
-
-async pushSheetsData(name, address, dob, fileLink, sheet = 'Sheet1') {
-    const googleSheets = google.sheets({version: 'v4', auth: await this.getSheetsAuth()});
-    const resource = { values: [[name, address, dob, fileLink]] };
-
-    googleSheets.spreadsheets.values.append({
-        auth: this.auth,
-        spreadsheetId: this.spreedsheetId,
-        range: sheet,
-
-        valueInputOption: 'USER_ENTERED',
-        resource
-    });
-}
-
-async uploadDrive(file) {
-    const googleDrive = google.drive({version: 'v3', auth: await this.getDriveAuth()});
-    let stream = require('stream');
-    let bufferStream = new stream.PassThrough();
-    
-    const fileMetaData = {
-        name: file.originalname,
-        parents: [this.folderId]
+    async getSheetsRows(sheet = 'Sheet1') {
+        const googleSheets = google.sheets({version: 'v4', auth: await this.getSheetsAuth()});
+        return (await googleSheets.spreadsheets.values.get({
+            auth: this.auth,
+            spreadsheetId: this.spreedsheetId,
+            range: sheet
+        })).data;
     }
 
-    const media = {
-        mimeType: file.mimetype,
-        body: bufferStream.end(file.buffer)
+    async pushSheetsData(name, address, dob, fileLink, sheet = 'Sheet1') {
+        const googleSheets = google.sheets({version: 'v4', auth: await this.getSheetsAuth()});
+        const resource = { values: [[name, address, dob, fileLink]] };
+
+        googleSheets.spreadsheets.values.append({
+            auth: this.auth,
+            spreadsheetId: this.spreedsheetId,
+            range: sheet,
+
+            valueInputOption: 'USER_ENTERED',
+            resource
+        });
     }
 
-    const response = await googleDrive.files.create({
-        resource: fileMetaData,
-        media,
-        fields: 'id'
-    });
+    async uploadDrive(file) {
+        const googleDrive = google.drive({version: 'v3', auth: await this.getDriveAuth()});
+        let stream = require('stream');
+        let bufferStream = new stream.PassThrough();
+        
+        const fileMetaData = {
+            name: file.originalname,
+            parents: [this.folderId]
+        }
 
-    const metaData = await googleDrive.files.get({
-        fileId: response.data.id,
-        fields: 'webViewLink'
-    });
+        const media = {
+            mimeType: file.mimetype,
+            body: bufferStream.end(file.buffer)
+        }
 
-    return metaData.data.webViewLink;
-}
+        const response = await googleDrive.files.create({
+            resource: fileMetaData,
+            media,
+            fields: 'id'
+        });
+
+        const metaData = await googleDrive.files.get({
+            fileId: response.data.id,
+            fields: 'webViewLink'
+        });
+
+        return metaData.data.webViewLink;
+    }
 }
 
 module.exports = {

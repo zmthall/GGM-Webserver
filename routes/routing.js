a51d2b8daeb5c612f41b9a36c6c64d55c1dcc9e0
zmthall <zmthall@gmail.com>
2024-10-17 10:16:34 -0600

    Added meta descriptions to every routing page.

--- routes/routing.js
+++ routes/routing.js
@@ -34,6 +34,7 @@ router.get('/', (request, response) => {
         helper: helper,
         page: {
             title: 'Medicaid Transportation',
+            meta_description: "Golden Gate Manor offers reliable Medicaid-certified transportation, assisted living, and medical supplies in Pueblo. Improve your quality of life with our trusted services.",
             href: "/",
             page_type: "home-page"
         },
@@ -47,6 +48,7 @@ router.get('/accessible-vans', (request, response) => {
         helper: helper,
         page: {
             title: 'Accessible Transportation',
+            meta_description: "Golden Gate Manor offers wheelchair-accessible vans for reliable, barrier-free transportation. Enjoy safe, inclusive rides for medical appointments, errands, and more.",
             href: '/accessible-vans',
             page_type: "page"
         },
@@ -60,6 +62,7 @@ router.get('/assisted-living', (request, response) => {
         helper: helper,
         page: {
             title: 'Assisted Living',
+            meta_description: "Golden Gate Manor offers 24/7 care, medication management, and daily living assistance in Pueblo, CO. Schedule a tour today and explore our Medicaid-certified facilities.",
             href: '/assisted-living',
             page_type: "page"
         },
@@ -73,6 +76,7 @@ router.get('/non-medical-transportation', (request, response) => {
         helper: helper,
         page: {
             title: 'Non-Medical Transportation',
+            meta_description: "Golden Gate Manor provides reliable non-medical transportation for Medicaid recipients in Pueblo and Otero Counties. Call today to schedule your ride.",
             href: '/non-medical-transportation',
             page_type: "page"
         },
@@ -86,6 +90,7 @@ router.get('/nemt-transportation', (request, response) => {
         helper: helper,
         page: {
             title: 'Non-Emergency Medical Transportation',
+            meta_description: "Golden Gate Manor offers reliable NEMT services for Medicaid-approved appointments across Colorado. Call to schedule a ride today!",
             href: '/nemt-transportation',
             page_type: "page"
         },
@@ -99,6 +104,7 @@ router.get('/about-us', (request, response) => {
         helper: helper,
         page: {
             title: 'About Golden Gate Manor Inc.',
+            meta_description: "Discover Golden Gate Manor, a family-owned facility in Pueblo, CO, offering compassionate assisted living, NEMT, and medical supplies. Your quality of life is our mission!",
             href: '/about-us',
             page_type: "page"
         },
@@ -112,6 +118,7 @@ router.get('/about-us/location', (request, response) => {
         helper: helper,
         page: {
             title: 'Location',
+            meta_description: "Find Golden Gate Manor at 648 S. Union Ave., Pueblo, CO. Contact us at (719) 544-3231 for personalized care in assisted living, NEMT, and medical supplies.",
             href: '/about-us/location',
             page_type: "page"
         },
@@ -147,6 +154,7 @@ router.get('/about-us/community', (request, response) => {
             helper: helper,
             page: {
                 title: 'Community Outreach and Events',
+                meta_description: "Discover Golden Gate Manor's commitment to enhancing lives through community outreach. Learn about our support for local initiatives, upcoming events, and how we foster independence in Pueblo, CO.",
                 href: '/about-us/community',
                 page_type: "page"
             },
@@ -164,6 +172,7 @@ router.get('/about-us/employment', (request, response) => {
         helper: helper,
         page: {
             title: 'Employment Opportunities',
+            meta_description: "Join our dedicated team at Golden Gate Manor, where we value reliability, teamwork, and compassion. Explore job opportunities in transportation, assisted living, and medical supplies today!",
             href: '/about-us/employment',
             page_type: "page"
         },
@@ -177,6 +186,7 @@ router.get('/about-us/employment/apply', (request, response) => {
         helper: helper,
         page: {
             title: 'Golden Gate Manor - Application Form',
+            meta_description: "Apply to join our dedicated team at Golden Gate Manor! Complete our job application form for full-time or part-time positions and take the first step toward a rewarding career in compassionate care.",
             href: '/about-us/employment/apply',
             page_type: "page"
         },
@@ -217,6 +227,7 @@ router.get('/contact-us', (request, response) => {
         helper: helper,
         page: {
             title: 'Contact Us',
+            meta_description: "Get in touch with Golden Gate Manor! Reach out for inquiries about our transportation, assisted living, or medical supply services. We're here to help!",
             href: '/contact-us',
             page_type: "page"
         },
@@ -257,6 +268,7 @@ router.get('/contact-us/thank-you', (request, response) => {
         helper: helper,
         page: {
             title: 'Thank You',
+            meta_description: "Thank you for your submission to Golden Gate Manor! If you have questions about your inquiry, please contact us at (719) 544-3231. We're here to help!",
             href: '/contact-us/thank-you',
             page_type: "page"
         },
@@ -270,6 +282,7 @@ router.get('/contact-us/schedule-a-ride', (request, response) => {
         helper: helper,
         page: {
             title: 'Schedule a Ride',
+            meta_description: "Schedule your ride with Golden Gate Manor! Fill out our simple form to request non-medical or NEMT transportation services. Please allow 24-48 hours for trip requests.",
             href: '/contact-us',
             page_type: "page"
         },
@@ -283,6 +296,7 @@ router.get('/policy/privacy-policy', (request, response) => {
         helper: helper,
         page: {
             title: 'Privacy Policy',
+            meta_description: "Learn how Golden Gate Manor protects your personal information. Our Privacy Policy outlines data collection, usage, and your rights regarding privacy. Last updated June 11, 2024.",
             href: '/policy/privacy-policy',
             page_type: "policy-page"
         },
@@ -296,6 +310,7 @@ router.get('/policy/accessibility', (request, response) => {
         helper: helper,
         page: {
             title: 'Accessibility',
+            meta_description: "Learn about Golden Gate Manor's commitment to digital accessibility, ensuring all users can easily access our services and information. Contact us for assistance.",
             href: '/policy/accessibility',
             page_type: "policy-page"
         },
@@ -309,6 +324,7 @@ router.get('/site-map', (request, response) => {
         helper: helper,
         page: {
             title: 'Site Map',
+            meta_description: "Explore the Golden Gate Manor sitemap for easy navigation of our services, including Assisted Living, Non-Medical and NEMT Transportation, and more.",
             href: '/site-map',
             page_type: "page"
         },

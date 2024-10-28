c6c6ac5ce47e991112498a524e80e65ec33d4c7b
zmthall <zmthall@gmail.com>
2024-10-28 09:42:23 -0600

    uncommented some code

--- static/unminified-static/scripts/recaptcha.js
+++ static/unminified-static/scripts/recaptcha.js
@@ -22,23 +22,23 @@ class OnSiteRecaptcha {
 
             if(this.form.getAttribute('data-submittable') === '') {
                 if(hasAttestation) {
-                    // grecaptcha.ready(function() {
-                    //     grecaptcha.execute(_this.publicKey, {action: 'submit'}).then(async function(token) {
-                    //         const submitBtn = _this.form.querySelector('button');
-                    //         const data = _this.createData(token);
-                    //         const verify = await _this.verifySubmission(data)
-                    //         if(verify.status === 200) {
-                    //             _this.messageContainer.innerText = `Submission ${(await verify.json()).msg}, now redirecting...`
-                    //             submitBtn.disabled = true;
-                    //             setTimeout(() => {
-                    //                 const redirectURL = new URL('/contact-us/thank-you', window.location.origin);
-                    //                 window.location.replace(redirectURL);
-                    //             }, 2000);
-                    //         } else if(verify.status === 401) {
-                    //             _this.messageContainer.innerText = `Submission ${(await verify.json()).msg}...`
-                    //         }
-                    //     });
-                    // });
+                    grecaptcha.ready(function() {
+                        grecaptcha.execute(_this.publicKey, {action: 'submit'}).then(async function(token) {
+                            const submitBtn = _this.form.querySelector('button');
+                            const data = _this.createData(token);
+                            const verify = await _this.verifySubmission(data)
+                            if(verify.status === 200) {
+                                _this.messageContainer.innerText = `Submission ${(await verify.json()).msg}, now redirecting...`
+                                submitBtn.disabled = true;
+                                setTimeout(() => {
+                                    const redirectURL = new URL('/contact-us/thank-you', window.location.origin);
+                                    window.location.replace(redirectURL);
+                                }, 2000);
+                            } else if(verify.status === 401) {
+                                _this.messageContainer.innerText = `Submission ${(await verify.json()).msg}...`
+                            }
+                        });
+                    });
                 } else {
                     _this.messageContainer.innerText = `Form failed to be submitted... refreshing page...`;
                     setTimeout(() => {
